5de9774611aaec7d62661af3b17ae91e47845df1
atem228-gif <atem.228@mail.ru>
2025-11-15 13:37:09 +0500

    52

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+     b = input()
+     a = input()
+     print(a == "awesome" or b == "awesome")
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
--- tasks/task6.py
+++ tasks/task6.py
@@ -2,9 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-
-   
-   
+  a, b, c = map(int, input().split())
+  a, b, c = sorted([a, b, c])
+  print(c * c == a * a + b * b)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
